8f34d17b7f837b09e23bf3ae9959d078768dd6dc
Alicia Ramirez <aramirez@rrc.ca>
2016-09-16 15:47:37 -0500

    Loop, single
    Added a loop to the home page, body class to header, and added the
    single template with a loop.

--- header.php
+++ header.php
@@ -7,7 +7,7 @@
     <?php wp_head(); ?>
 
 </head>
-<body>
+<body <?php body_class(); ?>>
    <div class="row">
         <div class="cute-4-tablet">
             <img src="<?php echo get_stylesheet_directory_uri()."/images/placeholder.gif";?>" alt="<?php bloginfo('name'); ?>">
--- single.php
+++ single.php
@@ -1,12 +1,11 @@
 <?php get_header(); ?>
-
 <div class="row">
     <div class="cute-8-tablet">
-        <h1>Index.php</h1>
+        <h1>Single.php</h1>
         
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <article <?php post_class(); ?>>
-        <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+        <h1><?php the_title(); ?></h1>
         
         <p><?php the_time('jS \of F Y'); ?><p>
         <p>By <?php the_author_posts_link(); ?></p>
